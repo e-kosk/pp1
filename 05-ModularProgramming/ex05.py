baa3c1416cf0b8c99dff5a64651a349b8046898c
Eryk Kosk <eryk.kosk@wp.pl>
2019-12-05 15:00:31 +0100

    Exercises in folder 05.

--- 05-ModularProgramming/ex05.py
+++ 05-ModularProgramming/ex05.py
@@ -5,4 +5,4 @@ tab = [21600, 4350, 3920, 5590, 3250, 4010]
 
 print(f'Srednia: {s.mean(tab)}')
 print(f'Mediana: {s.median(tab)}')
-print(f'Odchylenie standardowe: {s.}')
+print(f'Odchylenie standardowe: {s.stdev(tab)}')
